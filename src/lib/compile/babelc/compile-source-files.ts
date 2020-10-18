d2c8828dd6a86ffa74b3e556c35c38f36d98ce43
ben <imben1109@gmail.com>
2020-10-18 18:26:54 +0800

    update comment

--- src/lib/compile/babelc/compile-source-files.ts
+++ src/lib/compile/babelc/compile-source-files.ts
@@ -19,6 +19,8 @@ const outputFolder = "dist/esm2015";
  * @returns output folder path
  */
 export async function compileSourceFiles(userPackage: UserPackage): Promise<string> {
+    
+    // search for files in src folder 
     const filePaths = glob.sync(
             userPackage.sourceFolderPath + '/**/*.*', {
             ignore: userPackage.sourceFolderPath + '/node_modules/**'
@@ -27,30 +29,45 @@ export async function compileSourceFiles(userPackage: UserPackage): Promise<stri
 
     const promises: Promise<BabelFileResult>[] = [];
     for (const sourceFilePath of filePaths){
+
         if (supportedFileSuffix.indexOf(path.extname(sourceFilePath)) > -1){
+            // file to be compiled
             console.log('starting to compile: ' + sourceFilePath);
 
             const transformOptions: TransformOptions = {
                 presets: ['@babel/preset-typescript', '@babel/preset-react']
             };
-    
             const promiseResult: Promise<BabelFileResult> = transformFileAsync(sourceFilePath, transformOptions);
             promises.push(promiseResult);
-            promiseResult.then((res: BabelFileResult) => {            
+            promiseResult.then((res: BabelFileResult) => {
+                
+                // evaluate the dest file path for the source file path
                 const relativeCompileFilePath  = getCompileFilePath(userPackage.destCompiledFolderPath, userPackage.sourceFolderPath, sourceFilePath, '.js');
                 console.log(`starting to write compiled file: ${relativeCompileFilePath}`);
+
+                // make folder if not exist
                 mkdirp.sync(path.dirname(relativeCompileFilePath));
+
+                // write the compiled javascript to file
                 fs.writeFile(relativeCompileFilePath, res.code, function(err){
                     if (err){
                         console.log(err);
                     }
                 });
             });
+
         } else {
+            // file not to be compiled
             console.log(`The file is not in supported file extension ${sourceFilePath} and starting to copy`);
+            
+            // evluate the dest file path the source file path
             const relativeFilePath  = getCompileFilePath(userPackage.destCompiledFolderPath, userPackage.sourceFolderPath, sourceFilePath, path.extname(sourceFilePath));
             console.log(`starting to copy file to: ${relativeFilePath}`)
+            
+            // make folder if not exist
             mkdirp.sync(path.dirname(relativeFilePath));
+
+            // copy the file to dest file path
             fs.copyFile(sourceFilePath, relativeFilePath, (err)=>{
                 if (err){
                     console.log(err);
@@ -60,6 +77,7 @@ export async function compileSourceFiles(userPackage: UserPackage): Promise<stri
         }
     }
     
+    // return output folder path once all source files are processed
     return new Promise((resolve) => {
         Promise.all(promises).then(()=>{
             resolve(outputFolder)
